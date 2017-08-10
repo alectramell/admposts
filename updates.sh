1f3b6d1da1e0d11211290ff4d9ce5f38f673b862
alectramell <alectramell@gmail.com>
2017-08-09 17:26:53 -0700

    admposts

--- updates.sh
+++ updates.sh
@@ -16,13 +16,25 @@ clear
 
 UPDATE() {
 
+	if [ -d /home/$(whoami)/.admicons ] || [ -e /home/$(whoami)/.admicons/adm.svg ]
+	then
+		clear
+	else
+		mkdir /home/$(whoami)/.admicons
+		clear
+		wget https://raw.githubusercontent.com/alectramell/admposts/master/icon.svg -O /home/$(whoami)/.admicons/adm.svg
+		clear
+
+	fi
+
 	clear
+
 	STATUS=$(curl -s https://raw.githubusercontent.com/alectramell/admposts/master/post.txt)
 	clear
 	sleep 5
 	clear
 	echo "${blue}Apollon Data Metrics (AGY): ${reset}$STATUS"
-	notify-send --urgency="critical" --icon="https://raw.githubusercontent.com/alectramell/admposts/master/icon.svg" "$STATUS"
+	notify-send --urgency="critical" --icon="/home/$(whoami)/.admicons/adm.svg" "$STATUS"
 }
 
 clear
