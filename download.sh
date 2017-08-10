ed076638fb0b6f6ddc202c1c7b01c017a217b340
alectramell <alectramell@gmail.com>
2017-08-09 18:09:28 -0700

    admposts

--- download.sh
+++ download.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if [ -e /home/$(whoami)/.admapps/admapp.sh ]
-then
-	bash /home/$(whoami)/.admapps/admapp.sh
-else
-	mkdir /home/$(whoami)/.admapps
-	wget https://github.com/alectramell/admposts/raw/master/main.sh -O /home/$(whoami)/.admapps/admapp.sh
-	clear
-	sleep 0.5
-	clear
-	bash /home/$(whoami)/.admapps/admapp.sh
-fi
+GETAPP() {
+
+	if [ -e /home/$(whoami)/.admapps/admapp.sh ]
+	then
+		bash /home/$(whoami)/.admapps/admapp.sh
+	else
+		mkdir /home/$(whoami)/.admapps
+		wget https://github.com/alectramell/admposts/raw/master/main.sh -O /home/$(whoami)/.admapps/admapp.sh
+		clear
+		sleep 0.5
+		clear
+		bash /home/$(whoami)/.admapps/admapp.sh
+	fi
+}
+
+$1
